6df1a0e95c3702b4dad7e54d0764795ba8294a1e
ROkelbow <kelbowow@gmail.com>
2024-04-15 18:29:49 +0330

    Update build.js

--- sam/build/build.js
+++ sam/build/build.js
@@ -2,116 +2,83 @@ const fs = require('fs');
 const path = require('path');
 const zlib = require('zlib');
 const rollup = require('rollup');
-const uglify = require('uglify-js');
-const uglifyEs = require('uglify-es');
+const { minify } = require('uglify-es');
+const { promisify } = require('util');
 
-if (!fs.existsSync('dist')) {
-  fs.mkdirSync('dist')
-}
+const writeFileAsync = promisify(fs.writeFile);
 
-let builds = require('./config').getAllBuilds();
-
-// filter builds via command line arg
-if (process.argv[2]) {
-  const filters = process.argv[2].split(',');
-  builds = builds.filter(b => {
-    return filters.some(f => b.dest.indexOf(f) > -1)
-  })
-}
-
-build(builds);
-
-function build (builds) {
-  let built = 0;
-  const total = builds.length;
-  const next = () => {
-    buildEntry(builds[built]).then(() => {
-      built++;
-      if (built < total) {
-        next()
-      }
-    }).catch(logError)
-  };
-
-  next()
-}
-
-function buildEntry (config) {
-  const outConfig = config.output
-  delete config.output
-  const isProd = /min\.js$/.test(outConfig.file);
-  const build = async () => {
-    const bundle = await rollup.rollup(config)
-    const {output} = await bundle.generate(outConfig)
-    const code = output[0].code
-    if (isProd) {
-      let result = (/esm\.min$/.test(outConfig.file) ? uglify : uglifyEs).minify(code, {
-        mangle: {
-          // keep_classnames: true,
-        },
-        warnings: true,
-        toplevel: true,
-        output: {
-          ascii_only: true
-        },
-        compress: {
-          properties: true,
-          dead_code: true,
-          conditionals: true,
-          reduce_vars: true,
-          keep_fnames: true
-        },
-        sourceMap: {
-          filename: path.basename(outConfig.file),
-          url: path.basename(outConfig.file) + '.map'
-        }
-      });
-      let minimized = (outConfig.banner ? outConfig.banner + '\n' : '') + result.code;
-      if (result.error) console.error(result.error.message);
-      if (result.warnings) console.warn(result.warnings);
-
-      await Promise.all([
-        write(outConfig.file, minimized, true),
-        write(outConfig.file + '.map', result.map || '', true)
-      ]);
-    } else {
-      await write(outConfig.file, code)
-    }
+async function build(builds) {
+  if (!fs.existsSync('dist')) {
+    fs.mkdirSync('dist');
   }
-  return new Promise((resolve, reject) => {
-    build().then(() => resolve()).catch(reason => reject(reason))
-  })
+
+  for (const config of builds) {
+    await buildEntry(config);
+  }
 }
 
-function write (dest, code, zip) {
-  return new Promise((resolve, reject) => {
-    function report (extra) {
-      console.log(blue(path.relative(process.cwd(), dest)) + ' ' + getSize(code) + (extra || ''));
-      resolve()
-    }
+async function buildEntry(config) {
+  const outConfig = config.output;
+  delete config.output;
+  const isProd = /min\.js$/.test(outConfig.file);
 
-    fs.writeFile(dest, code, err => {
-      if (err) return reject(err);
-      if (zip) {
-        zlib.gzip(code, (err, zipped) => {
-          if (err) return reject(err);
-          report(' (gzipped: ' + getSize(zipped) + ')')
-        })
-      } else {
-        report()
-      }
-    })
-  })
+  const bundle = await rollup.rollup(config);
+  const { output } = await bundle.generate(outConfig);
+  const code = output[0].code;
+
+  if (isProd) {
+    const result = minify(code, {
+      mangle: {
+        // keep_classnames: true,
+      },
+      warnings: true,
+      toplevel: true,
+      output: {
+        ascii_only: true,
+      },
+      compress: {
+        properties: true,
+        dead_code: true,
+        conditionals: true,
+        reduce_vars: true,
+        keep_fnames: true,
+      },
+      sourceMap: {
+        filename: path.basename(outConfig.file),
+        url: path.basename(outConfig.file) + '.map',
+      },
+    });
+
+    let minimized = (outConfig.banner ? outConfig.banner + '\n' : '') + result.code;
+    if (result.error) console.error(result.error.message);
+    if (result.warnings) console.warn(result.warnings);
+
+    await Promise.all([
+      write(outConfig.file, minimized, true),
+      write(outConfig.file + '.map', result.map || '', true),
+    ]);
+  } else {
+    await write(outConfig.file, code);
+  }
 }
 
-function getSize (code) {
-  return (code.length / 1024).toFixed(2) + 'kb'
+async function write(dest, code, zip) {
+  await writeFileAsync(dest, code);
+  if (zip) {
+    const zipped = zlib.gzipSync(code);
+    console.log(`${blue(path.relative(process.cwd(), dest))} ${getSize(code)} (gzipped: ${getSize(zipped)})`);
+    await writeFileAsync(dest + '.gz', zipped);
+  } else {
+    console.log(`${blue(path.relative(process.cwd(), dest))} ${getSize(code)}`);
+  }
 }
 
-function logError (e) {
-  console.log(e)
+function getSize(code) {
+  return (code.length / 1024).toFixed(2) + 'kb';
 }
 
-function blue (str) {
-  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+function blue(str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m';
 }
+
+module.exports = build;
