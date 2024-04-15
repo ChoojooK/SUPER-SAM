b8685b9b113871a6de8d97bd8c5864c75bb53042
ROkelbow <kelbowow@gmail.com>
2024-04-15 18:42:05 +0330

    Update config.js

--- sam/build/config.js
+++ sam/build/config.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const buble = require('@rollup/plugin-buble');
-const replace = require('@rollup/plugin-replace');
-const flow = require('rollup-plugin-flow-no-whitespace');
-const version = process.env.VERSION || require('../package.json').version;
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const { babel } = require('@rollup/plugin-babel');
+const { terser } = require('rollup-plugin-terser');
+const { version } = require('../package.json');
 
 const banner =
   '/**\n' +
@@ -34,85 +34,50 @@ const buildSrc = [
     dest: 'guessnum',
     moduleName: 'GuessNum',
   },
-]
+];
 
-const builds = {}
+const builds = {};
 
-buildSrc.map((file) => {
-  [
-    // Browser build
-    {
-      name: '',
-      suffix: '',
-      format: 'umd',
-    },
-    // CommonJS. Used by bundlers e.g. Webpack & Browserify
-    {
-      name: 'cjs',
-      suffix: '.common',
-      format: 'cjs',
-    },
-    // ES Modules. Used by bundlers that support ES Modules, e.g. Rollup & Webpack 2
-    {
-      name: 'esm',
-      suffix: '.esm',
-      format: 'es',
-    },
-  ].map((build) => {
-    const name = (build.name ? '-' + build.name : '') + (file.name ? '-' + file.name : '')
-    builds['dev' + name] = {
-      entry: resolve(file.entry),
-      dest: resolve(`dist/${file.dest}${build.suffix}.js`),
-      moduleName: file.moduleName,
-      format: build.format,
-      env: 'development',
-      banner
-    }
-    builds['prod' + name] = {
-      entry: resolve(file.entry),
-      dest: resolve(`dist/${file.dest}${build.suffix}.min.js`),
-      moduleName: file.moduleName,
-      format: build.format,
-      env: 'production',
-      banner
-    }
-  })
-})
+buildSrc.forEach((file) => {
+  ['umd', 'cjs', 'esm'].forEach((format) => {
+    const nameSuffix = file.name ? '-' + file.name : '';
+    const buildName = `${format}${nameSuffix}`;
 
-function genConfig (opts) {
-  let moduleName = opts.moduleName;
-  const config = {
-    input: opts.entry,
-    output: {
-      file: opts.dest,
-      format: opts.format,
-      name: moduleName,
-      banner: opts.banner,
-    },
-    external: opts.external,
-    plugins: [
-      replace({
-        __VERSION__: version
-      }),
-      flow(),
-      buble()
-    ].concat(opts.plugins || [])
-  };
+    builds[`dev-${buildName}`] = createConfig(file, format, 'development');
+    builds[`prod-${buildName}`] = createConfig(file, format, 'production');
+  });
+});
 
-  if (opts.env) {
-    config.plugins.push(replace({
-      'process.env.NODE_ENV': JSON.stringify(opts.env),
-      'process.env.DEBUG_SAM': JSON.stringify(opts.env === 'development'),
-    }))
+function createConfig(file, format, env) {
+  const nameSuffix = file.name ? '-' + file.name : '';
+  const suffix = format === 'umd' ? '' : `.${format}`;
+  const entry = resolve(file.entry);
+  const dest = resolve(`dist/${file.dest}${suffix}.js`);
+  const moduleName = file.moduleName;
+
+  const plugins = [
+    nodeResolve(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
+  ];
+
+  if (env === 'production') {
+    plugins.push(terser());
   }
 
-  return config
+  return {
+    input: entry,
+    output: {
+      file: dest,
+      format: format,
+      name: moduleName,
+      banner: banner,
+    },
+    plugins: plugins,
+    external: [],
+  };
 }
 
-if (process.env.TARGET) {
-  module.exports = genConfig(builds[process.env.TARGET])
-} else {
-  exports.getBuild = name => genConfig(builds[name]);
-  exports.getAllBuilds = () => Object.keys(builds).map(name => genConfig(builds[name]))
-}
-
+module.exports = builds;
